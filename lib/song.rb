10853c617173b0937ced3df3e2e75309020751df
Ray Alva <rayalva407@gmail.com>
2020-04-03 20:18:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ class Song
     song
   end
   
-  def self.create_from_filename
+  def self.create_from_filename(filename)
     song = self.new
     song.name = filename.split(" - ")[1].chomp(".mp3")
     song.artist_name = filename.split(" - ").chomp(".mp3")
